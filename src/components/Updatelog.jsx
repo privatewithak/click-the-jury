cdfb9f571bfe10e3361563329d085200cc690442
privatewithak <head4shotter@gmail.com>
2025-12-29 04:08:57 +0300

    fix blur on the updatelog

--- src/components/Updatelog.jsx
+++ src/components/Updatelog.jsx
@@ -1,6 +1,6 @@
 export default function Updatelog({ theme }) {
   return (
-    <div className={`p-4 mb-4 mt-12 rounded-2xl border border-white/10 bg-white/5 backdrop-blur-xl text-sm max-h-64 overflow-y-auto w-[80%] sm:w-full max-w-md`}>
+    <div className='relative p-4 mb-4 mt-12 rounded-2xl border border-white/10 bg-white/5 backdrop-blur-xl text-sm max-h-64 w-[80%] sm:w-full max-w-md z-2 mx-auto'>
           <h2 className="text-lg font-semibold font-mono">update log</h2>
           <p className={`mb-2 ${theme.textSoft}`}>29.12.2025 v.1.2</p>
       <ul className="list-disc list-inside space-y-1">
